0f43cad54654538385060c03d91201a0a17e02f1
rmiranda <rmiranda@student.42sp.org.br>
2022-12-18 06:12:46 +0100

    Norminette OK and removed debug printfs

--- utils/pusw_perform_radix.c
+++ utils/pusw_perform_radix.c
@@ -6,58 +6,67 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 03:22:10 by rmiranda          #+#    #+#             */
-/*   Updated: 2022/12/18 05:52:21 by rmiranda         ###   ########.fr       */
+/*   Updated: 2022/12/18 06:11:26 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	perform_radix(t_node **stack_a, const int counter)
+static void	sort_n_byte_signed(t_node **stack_a, int byte_position)
 {
 	t_node	*reference_node;
 	t_node	*stack_b;
 
 	stack_b = NULL;
-	if (counter == 7)
+	reference_node = (*stack_a)->previus;
+	while (reference_node != (*stack_a))
 	{
-		ft_printf("bit number %i ---\n", counter);
-		output_stack(*stack_a);
-		reference_node = (*stack_a)->previus;
-		while (reference_node != (*stack_a))
-		{
-			if (((*stack_a)->value >> counter) & 1)
-				pusw_pb(stack_a, &stack_b);
-			else
-				pusw_ra(stack_a);
-		}
-		if (((*stack_a)->value >> counter) & 1)
+		if (((*stack_a)->value >> byte_position) & 1)
 			pusw_pb(stack_a, &stack_b);
 		else
 			pusw_ra(stack_a);
-		ft_printf("<-\n");
-		while (stack_b)
-			pusw_pa(stack_a, &stack_b);
+	}
+	if (((*stack_a)->value >> byte_position) & 1)
+		pusw_pb(stack_a, &stack_b);
+	else
+		pusw_ra(stack_a);
+	ft_printf("<-\n");
+	while (stack_b)
+		pusw_pa(stack_a, &stack_b);
+}
+
+static void	sort_n_byte(t_node **stack_a, int byte_position)
+{
+	t_node	*reference_node;
+	t_node	*stack_b;
+
+	stack_b = NULL;
+	reference_node = (*stack_a)->previus;
+	while (reference_node != *stack_a)
+	{
+		if (((*stack_a)->value >> byte_position) & 1)
+			pusw_ra(stack_a);
+		else
+			pusw_pb(stack_a, &stack_b);
+	}
+	if (((*stack_a)->value >> byte_position) & 1)
+		pusw_ra(stack_a);
+	else
+		pusw_pb(stack_a, &stack_b);
+	while (stack_b)
+		pusw_pa(stack_a, &stack_b);
+}
+
+void	perform_radix(t_node **stack_a, const int counter)
+{
+	if (counter == 7)
+	{
+		sort_n_byte_signed(stack_a, counter);
 		return ;
 	}
 	else
 	{
-		ft_printf("bit number %i ---\n", counter);
-		output_stack(*stack_a);
-		reference_node = (*stack_a)->previus;
-		while (reference_node != *stack_a)
-		{
-			if (((*stack_a)->value >> counter) & 1)
-				pusw_ra(stack_a);
-			else
-				pusw_pb(stack_a, &stack_b);
-		}
-		if (((*stack_a)->value >> counter) & 1)
-			pusw_ra(stack_a);
-		else
-			pusw_pb(stack_a, &stack_b);
-		ft_printf("<-\n");
-		while (stack_b)
-			pusw_pa(stack_a, &stack_b);
+		sort_n_byte(stack_a, counter);
 		perform_radix(stack_a, counter + 1);
 	}
 }
